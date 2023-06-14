3ad8655ab9309473056884bd6057ae079a265675
Anika <tabassumanika210@gmail.com>
2023-06-14 09:22:38 +0600

    instructor page complete

--- src/Pages/AllTeacher/AllTeacher.jsx
+++ src/Pages/AllTeacher/AllTeacher.jsx
@@ -4,10 +4,11 @@ const AllTeacher = ({item1}) => {
     return (
         <div className="flex space-x-4">
             <figure className="px-3 pt-5">
-            <img className="object-cover w-[150px] h-[150px] mt-0"  src={picture}  alt="" />
+            <img className="object-cover w-[160px] h-[150px] mt-0"  src={picture}  alt="" />
             </figure>
-            <div className="mt-7">
-                <h3 className="3xl text-lime-600">Name:{name}</h3>
+            <div className="mt-9">
+                <h3 >
+                    Name:<span className="4xl text-green-600 ">{name}</span></h3>
                 <p>Email:{email}</p>
                 <p>Class:{Class}</p>
                 <p>Catagory:{Catagory}</p>
