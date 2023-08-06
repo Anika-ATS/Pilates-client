b47e5810c1e5cd6c4eedd9e2e0f0a10221e56744
Anika <tabassumanika210@gmail.com>
2023-08-07 03:05:50 +0600

    when user is admin or instructor they can't select any class

--- src/Pages/AllClasses/DisPAllcls.jsx
+++ src/Pages/AllClasses/DisPAllcls.jsx
@@ -1,55 +1,71 @@
 import Swal from 'sweetalert2'
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-const DisPAllcls = ({Acls}) => {
-    const {_id, Iname,name,photo,price,seat} = Acls;
+const DisPAllcls = ({ Acls,loggedIn, role }) => {
+    const { _id, Iname, name, photo, price, seat } = Acls;
     const navigate = useNavigate();
 
-  
-    const handleApprove =Acls=> {
 
-        fetch('https://y-opal-kappa.vercel.app/SelectCls',{
-            method:'POST',
-            headers:{
-               'content-type':'application/json'
+
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+    useEffect(() => {
+        // Disable the button if available seats are 0
+        if (seat === 0) {
+          setIsButtonDisabled(true);
+        }
+    
+        // If the user is logged in as admin or instructor, disable the button
+        if (loggedIn && (role === 'admin' || role === 'instructor')) {
+          setIsButtonDisabled(true);
+        }
+      }, [seat, loggedIn, role]);
+
+
+    const handleApprove = Acls => {
+
+        fetch('https://y-opal-kappa.vercel.app/SelectCls', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
             },
-            body:JSON.stringify(Acls)
+            body: JSON.stringify(Acls)
         })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            if(data.insertedId)
-            {
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'Your work has been saved',
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-            }
-            else {
-                // Handle the case where data.insertedId is falsy or not present
-                // This could mean an error occurred or the response was unexpected
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your work has been saved',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+                else {
+                    // Handle the case where data.insertedId is falsy or not present
+                    // This could mean an error occurred or the response was unexpected
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong!',
+                    });
+                }
+
+            })
+            .catch(error => {
+                // Handle any errors that occurred during the fetch operation
+                console.error('Error:', error);
                 Swal.fire({
                     icon: 'error',
                     title: 'Oops...',
-                    text: 'Something went wrong!',
+                    text: 'Something went wrong! Please login',
                 });
-            }
+                navigate('/login');
 
-        })
-        .catch(error => {
-            // Handle any errors that occurred during the fetch operation
-            console.error('Error:', error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Something went wrong! Please login',
             });
-            navigate('/login');
-       
-        });
 
     }
 
@@ -57,18 +73,31 @@ const DisPAllcls = ({Acls}) => {
 
     return (
         <div>
-           
+
             <div className="hero h-[500px]  mx-auto bg-base-200 mt-7 mb-7 ">
-                <div  className=" hero-content flex-col lg:flex-row-reverse">
+                <div className=" hero-content flex-col lg:flex-row-reverse">
                     <img src={photo} className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
                         <h1 className=" text-3xl font-bold mt-3 text-lime-600 mb-3">Class Name:{name}</h1>
                         <p className="text-2xl py-2">Instructor Name:{Iname}</p>
-                       
+
                         <p className=" text-2xl py-2">Available Seat:{seat}</p>
                         <p className=" text-2xl py-2">Course Price:{price}</p>
                         {/* select  for add to cart */}
-                        <button onClick={() => handleApprove(Acls)} className="btn btn-primary">Select</button>
+                        <button onClick={handleApprove} className="btn btn-primary" disabled={isButtonDisabled}>
+                            {loggedIn && (role === 'admin' || role === 'instructor')
+                                ? 'Admin/Instructor: Not Available'
+                                : seat === 0
+                                    ? 'Not Available'
+                                    : 'Select'}
+                        </button>
+
+
+
+
+
+
+
                         {/* <input onClick={() => handleApprove(select)}
                         className=" btn btn-primary btn-block text-xl"                         
                         type='submit'
@@ -78,7 +107,7 @@ const DisPAllcls = ({Acls}) => {
                     </div>
                 </div>
             </div>
-            
+
         </div>
     );
 };
--- src/Pages/Login/Register.jsx
+++ src/Pages/Login/Register.jsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -11,166 +11,226 @@ const Register = () => {
         register,
         handleSubmit,
         reset,
+        control,
+        watch,
+
 
         formState: { errors },
     } = useForm();
-       const navigate=useNavigate();
-       const {createUser,udateUserProfile,logOut}=useContext(AuthContext);
+    const navigate = useNavigate();
 
 
-    const onSubmit = (data) =>{ 
+
+
+    const { createUser, udateUserProfile, logOut } = useContext(AuthContext);
+    const selectedCategory = watch("category");
+
+    const onSubmit = (data) => {
         // console.log(data),
         //create user
-         createUser(data.email,data.password)
-            .then(result=>{
-                const createdUser=result.user;
-                console.log(createdUser);
-                udateUserProfile(data.name,data.photo)
-                .then(() => {
-                   
-                    // created user manage  in db
-                    const saveUser={name:data.name,email:data.email,photo:data.photo}
-                    // console.log(saveUser)
-                    fetch('https://y-opal-kappa.vercel.app/users',{
-                        method:'POST',
-                        headers:{
-                           'content-type':'application/json'
-                        },
-                        body:JSON.stringify(saveUser)
-                    }
-                                       
-                    )
-                    .then(res=>res.json())
-                    .then(data=>{
+        let newUser;
+        createUser(data.email, data.password)
+            .then(result => {
+                const createdUser = result.user;
+                // console.log(createdUser);
+                let url;
+                udateUserProfile(createdUser, data.name, url)
+                    .then(() => {
 
-                        if(data.insertedId){
-                            // console.log('Profile updated!');
-                            reset();
-                            logOut()
-                            .then(()=>{
-                                navigate ('/login');
-                            })
-                            .catch(error => console.log(error));
-
-
-
+                        if (selectedCategory == "instructor") {
+                            newUser = {
+                                name: data.name,
+                                email: data.email,
+                                image: url,
+                                role: data.category,
+                             
+                                
+                            };
+                        } else {
+                            newUser = {
+                                name: data.name,
+                                email: data.email,
+                                image: url,
+                                role: data.category,
+                              
+                            };
                         }
+
+                        // created user manage  in db
+                        // const saveUser = { name: data.name, email: data.email, photo: data.photo }
+                        // console.log(saveUser)
+                        fetch('https://y-opal-kappa.vercel.app/users', {
+                            method: 'POST',
+                            headers: {
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify(newUser)
+                        }
+
+                        )
+                            .then(res => res.json())
+                            .then(data => {
+
+                                if (data.insertedId) {
+                                    // console.log('Profile updated!');
+                                    reset();
+                                    logOut()
+                                        .then(() => {
+                                            navigate('/login');
+                                        })
+                                        .catch(error => console.log(error));
+
+
+
+                                }
+                            })
+
                     })
-  
-                  })
-                  .catch(error =>  console.log(error));
-                    
+                    .catch(error => console.log(error));
+
             })
-            .catch(error=>{
+            .catch(error => {
                 console.log(error);
             })
-    
-    
-    
-    }
-
-
-
- 
-    //getting form value
-    // const handleSignUp= event =>{
-    //     event.preventDefault();
-    //     const form=event.target;
-    //     const name=form.name.value;
-    //     const email=form.email.value;
-
-    //     const password=form.password.value;
-    //     const photo=form.photo.value;
-
-    //    console.log(name,password,email, photo);
-
-    //     //create user
-    //      createUser(email,password)
-    //         .then(result=>{
-    //             const createdUser=result.user;
-    //             console.log(createdUser);
-    //         })
-    //         .catch(error=>{
-    //             console.log(error);
-    //         })
-    //    }
 
 
 
 
 
 
-    return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <figure className="px-5 pt-5"> <img src={registerPic} alt="" className="rounded-xl" />
-                    </figure>
-                    <h1 className="ps-5 text-xl font-bold">Have an account?<br />You just click <Link className='link link:hover text-purple-800' to='/login'>Login</Link> for Sign-in!</h1>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form   onSubmit={handleSubmit(onSubmit)} className="card-body">
-                        <div className="form-control ">
-                           
-                            <input type="text"  {...register("name", { required: true })}  placeholder="Name" name='name' className="input input-bordered" />
-                            {errors.name && <span className="text-red-600">This field is required</span>}
-                        </div>
+        }
 
-                        {/* email */}
-                        <div className="form-control">
-                           
+        return (
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <figure className="px-5 pt-5"> <img src={registerPic} alt="" className="rounded-xl" />
+                        </figure>
+                        <h1 className="ps-5 text-xl font-bold">Have an account?<br />You just click <Link className='link link:hover text-purple-800' to='/login'>Login</Link> for Sign-in!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+
+
+                            {/* catagory */}
+                            <div>
+                                <label className="mx-2 text-[#1d2939]">Category:</label>
+                                <Controller
+                                    name="category"
+                                    control={control}
+                                    rules={{ required: true }}
+                                    render={({ field }) => (
+                                        <select
+                                            {...field}
+                                            defaultValue={"DEFAULT"}
+                                            onChange={e => {
+                                                field.onChange(e);
+                                                handleCategoryChange(e.target.value);
+                                            }}
+                                            className="mt-2  w-full input input-bordered"
+                                        >
+                                            <option value="DEFAULT" disabled>
+                                                Please select a category
+                                            </option>
+                                            <option value="Instructor">Doctor</option>
+                                            <option value="Student">Patient</option>
+                                        </select>
+                                    )}
+                                />
+                                {errors.category && (
+                                    <span className="text-red-600">Please select a category.</span>
+                                )}
+                            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+                            <div className="form-control ">
+
+                                <input type="text"  {...register("name", { required: true })} placeholder="Name" name='name' className="input input-bordered" />
+                                {errors.name && <span className="text-red-600">This field is required</span>}
+                            </div>
+
+                            {/* email */}
+                            <div className="form-control">
+
                                 <input type="email" {...register("email", { required: true })} placeholder="email" name='email' className="input input-bordered" />
-                            {errors.email && <span className="text-red-600">This field is required</span>}
-                        </div>
+                                {errors.email && <span className="text-red-600">This field is required</span>}
+                            </div>
 
-                        {/* password */}
-                        <div className="form-control">                          
-                            <input type="password" {...register("password", {
-                                required: true,
-                                minLength:6 ,
-                                maxLength:10,
-                                pattern:/(?=.*[A-Z])(?=.*[!@#$%&*])(?=.*[0-9].*[0-9].*[0-9])(?=.*[a-z])/   })} placeholder="Password" name='password' className="input input-bordered" />
-                                 {errors.password && <span className="text-red-600">This field is required</span>}
+                            {/* password */}
+                            <div className="form-control">
+                                <input type="password" {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 10,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$%&*])(?=.*[0-9].*[0-9].*[0-9])(?=.*[a-z])/
+                                })} placeholder="Password" name='password' className="input input-bordered" />
+                                {errors.password && <span className="text-red-600">This field is required</span>}
                                 {errors.password?.type === "minLength" && (
-                                 <p role="alert"  className="text-red-600">Password is less then 6 Characters</p>
+                                    <p role="alert" className="text-red-600">Password is less then 6 Characters</p>
                                 )}
                                 {errors.password?.type === "pattern" && (
-                                 <p role="alert" className="text-red-600">Must have One Capital letter, one special character ,3digit and one small letter </p>
+                                    <p role="alert" className="text-red-600">Must have One Capital letter, one special character ,3digit and one small letter </p>
                                 )}
-                           
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
 
-                         {/* Confirmpassword */}
-                         <div className="form-control">                          
-                            <input type="password" {...register("Confirmpassword", { required: true, minLength:6 , maxLength:10})} placeholder="Confirmpassword" name='Confirmpassword' className="input input-bordered" />
-                            {errors.Confirmpassword && <span className="text-red-600">This field is required</span>}
-                            
-                        </div>
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
 
-                        {/* photourl */}
-                        <div className="form-control">                                          
-                            <input type="url" {...register("photo", { required: true })} placeholder="Photo" name='photo' className="input input-bordered" />
-                            {errors.photo && <span className="text-red-600">This field is required</span>}
-                        </div>
+                            {/* Confirmpassword */}
+                            <div className="form-control">
+                                <input type="password" {...register("Confirmpassword", { required: true, minLength: 6, maxLength: 10 })} placeholder="Confirmpassword" name='Confirmpassword' className="input input-bordered" />
+                                {errors.Confirmpassword && <span className="text-red-600">This field is required</span>}
+
+                            </div>
+
+                            {/* photourl */}
+                            <div className="form-control">
+                                <label
+                                    htmlFor="fileInput"
+                                    className="text-teal-700 text-lg font-semibold group-hover:text-white"
+                                ></label>
+                                <input
+                                    type="file"
+                                    {...register("image", {
+                                        required: "Please select a file",
+                                    })}
+                                    id="fileInput"
+                                    name="image"
+                                    className="input input-bordered py-3 w-full "
+                                />
+                                {errors.file && (
+                                    <span className="text-red-600">{errors.file.message}</span>
+                                )}
+                            </div>
 
 
 
-                        <div className="form-control mt-6">
-                            <input  className="btn btn-primary"  type='submit'  value='Sign In' />
+
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type='submit' value='Sign In' />
                                 {/* <button className="btn btn-primary">Sign In</button> */}
-                        </div>
-                    </form>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </div >
+            </div >
 
 
 
 
-    );
-};
+        );
+    };
 
-export default Register;
+    export default Register;
